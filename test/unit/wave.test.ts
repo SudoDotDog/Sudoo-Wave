4c34778254d4cdce92599aa64897c20c93f02af9
WMXPY <wm@mengw.io>
2020-09-17 11:09:42 -0500

    add unit tests

---
+++ test/unit/wave.test.ts
@@ -0,0 +1,23 @@
+/**
+ * @author WMXPY
+ * @namespace Wave
+ * @description Wave
+ * @package Unit Test
+ */
+
+import { expect } from 'chai';
+import * as Chance from 'chance';
+import { Wave } from '../../src';
+
+describe('Given {Wave} Class', (): void => {
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const chance: Chance.Chance = new Chance('wave-wave');
+
+    it('should be able to construct', (): void => {
+
+        const wave: Wave = Wave.create();
+
+        expect(wave).to.be.instanceOf(Wave);
+    });
+});
