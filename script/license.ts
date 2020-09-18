b9b6b5cd7ed2fb76b8535dae53e583afae054447
WMXPY <wm@mengw.io>
2020-09-18 16:13:53 -0500

    ensure output

--- script/license.ts
+++ script/license.ts
@@ -26,5 +26,6 @@ const appPackage: any = {
     license: parent.license,
     bugs: parent.bugs,
     homepage: parent.homepage,
+    dependencies: parent.dependencies,
 };
 Fs.writeFileSync(Path.join(appPath, 'package.json'), JSON.stringify(appPackage, null, 2), 'utf8');
