01944072bd3f211776b0b7f64d65fd40c4113775
WMXPY <wm@mengw.io>
2020-09-22 10:36:22 -0500

    add update function

--- src/wave.ts
+++ src/wave.ts
@@ -4,6 +4,8 @@
  * @description Wave
  */
 
+import { compare, CompareResult } from "@sudoo/compare";
+
 export class Wave<T extends Record<string, any> = any> {
 
     public static create<T extends Record<string, any> = any>(initial: T): Wave<T> {
@@ -20,5 +22,12 @@ export class Wave<T extends Record<string, any> = any> {
 
     public get data(): T {
         return this._data;
+    }
+
+    public update(newData: T): CompareResult[] {
+
+        const changes: CompareResult[] = compare(this._data, newData);
+
+        return changes;
     }
 }
